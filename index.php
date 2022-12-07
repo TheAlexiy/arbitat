756c5d7fb1bb4e0a4d4c9a582f6f17d025860df6
Alex <diamatto2@gmail.com>
2022-12-07 17:34:22 +0400

    parsing everything and taking only the necessary stuff

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-type: text/html; charset=utf-8');
+
 require 'db.php';
 require('phpQuery/phpQuery/phpQuery.php');
 const HOST = 'http://www.arbitat.ru';
 
 $data_site = file_get_contents(HOST);
 $document = phpQuery::newDocument($data_site);
-$content_prev = $document->find("");
+$content_prev = $document->find('.grid .gridRow td');
+$text = pq($content_prev)->text();
+$text = htmlentities($text, null, 'utf-8');
+$text = str_replace("&nbsp;", " ", $text);
+$text = html_entity_decode($text);
+$text = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $text);
 
-//изучаю как пользоваться PhpQuery и парсить информацию
+$file = fopen('sandbox.txt', 'w');
+fwrite($file, $text);
+fclose($file);
 
-header('Location: save.php');
+$file = fopen('sandbox.txt', 'r');
+$lot = 0;
+$data[][] = array();
+while (!feof($file)) {
+    for ($i = 0; $i < 10; $i++) {
+        $data[$lot][$i] = trim(fgets($file));
+    }
+    $lot++;
+}
+fclose($file);
+echo $data[0][0];
+
+
+
